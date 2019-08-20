f64cd4432227785b4cbbefbcdd39ffe75992e4f2
Richard Yamamoto <rybolteri@gmail.com>
2019-08-20 09:56:13 -0300

    chore: Ambiente e Conceitos - Finalização Módulo01

--- index.js
+++ index.js
@@ -14,24 +14,55 @@ server.use(express.json());
 
 const users = ["Richard", "Amanda", "Paulo"];
 
+//Criando middleware global
+// //Manipulam as requisições de alguma forma
+
+server.use((req, res, next) => {
+  //Middleware de log das requisições
+  console.time("Request");
+  console.log("Requisição foi chamada");
+  console.log(`Método: ${req.method}; URL: ${req.url} `);
+
+  next();
+  console.timeEnd("Request");
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required" });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+  if (!users[req.params.index]) {
+    return res.status(400).json({ error: "User does not exists!" });
+  }
+  req.user = user;
+  return next();
+}
+
 server.get("/users", (req, res) => {
   return res.json(users);
 });
 
-server.get("/users/:index", (req, res) => {
+server.get("/users/:index", checkUserInArray, (req, res) => {
   // return res.send("Para mandar texto");
   //Desestruturação
   const { index } = req.params;
-  return res.json(users[index]);
+  return res.json(req.user);
 });
 
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
   users.push(name);
+
   return res.json(users);
 });
 
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", checkUserExists, checkUserInArray, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -40,4 +71,10 @@ server.put("/users/:index", (req, res) => {
   return res.json(users);
 });
 
-server.listen(3000);
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send("Usuário deletado com sucesso!");
+});
+
+server.listen(3001);
